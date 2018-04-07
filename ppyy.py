0d01e10301c8959ba07deee3025eb2aa39d04631
rafabermudez16 <38160762+rafabermudez16@users.noreply.github.com>
2018-04-07 13:39:28 -0500

    i
    i

---
+++ ppyy.py
@@ -0,0 +1,11 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:44:40) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> edad=18
+>>> nombre='Rafa'
+>>> print("hola,"nombre",como estas, tu edad  es : "edad"")
+SyntaxError: invalid syntax
+>>> SyntaxError: invalid syntax
+SyntaxError: invalid syntax
+>>> 
+>>> 
+>>> 
